2ba618b8803bb2502e1bd1ec22969d35b20336a1
carsonfrench <78115103+carsonfrench@users.noreply.github.com>
2023-02-21 22:22:07 +0000

    worked on dfa

--- dfa/src/main/scala/dfa/DFA.scala
+++ dfa/src/main/scala/dfa/DFA.scala
@@ -3,5 +3,8 @@ package dfa // leave this line in the file
 case class State(label: String):
     override def toString = label
 
+case class Transition(from: State, symbol: Char, to: State)
+
+class DFA (states: Set[State], transitions: Set[Transition], startState: State, acceptingStates: Set[State], )
 
 
