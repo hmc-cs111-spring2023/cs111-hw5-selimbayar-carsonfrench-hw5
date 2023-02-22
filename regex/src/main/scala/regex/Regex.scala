42b67567ffa9f3d5f1729295f78275bd5d1e8c3d
selimbayar <63806495+selimbayar@users.noreply.github.com>
2023-02-22 05:57:15 +0000

    Finished definitions and simplify

--- regex/src/main/scala/regex/Regex.scala
+++ regex/src/main/scala/regex/Regex.scala
@@ -8,6 +8,20 @@ package regex
 
 // Add your definitions here
 
+trait RegularLanguage
+
+case object Empty extends RegularLanguage
+
+case object Epsilon extends RegularLanguage
+
+case class Character(char: Char) extends RegularLanguage
+
+case class Union(l1: RegularLanguage, l2: RegularLanguage) extends RegularLanguage
+
+case class Concat(l1: RegularLanguage, l2: RegularLanguage) extends RegularLanguage
+
+case class Star(l: RegularLanguage) extends RegularLanguage
+
 /** *****************************************************************************
   * Derivatives
   *
@@ -15,7 +29,22 @@ package regex
   */
 
 /** Simplifies a regular language */
-def simplify(lang: RegularLanguage): RegularLanguage = ???
+def simplify(lang: RegularLanguage): RegularLanguage = lang match
+  case Concat(Epsilon, lang) => simplify(lang)
+  case Concat(lang, Epsilon) => simplify(lang)
+  case Concat(Empty, lang) => simplify(Empty)
+  case Concat(lang, Empty) => simplify(Empty)
+  case Concat(l1, l2) => Concat(simplify(l1), simplify(l2))
+
+  case Union(Empty, lang) => simplify(lang)
+  case Union(lang, Empty) => simplify(lang)
+  case Union(l1, l2) => Union(simplify(l1), simplify(l2))
+
+  case Star(Epsilon) => Epsilon
+  case Star(Empty) => Empty
+  case Star(lang) => Star(simplify(lang)) 
+
+  case _ => lang
 
 /** A language is nullable if it contains ε */
 def nullable(lang: RegularLanguage): Boolean = ???
